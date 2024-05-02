17bbd5a908f0d9849dcebc6fd669e84f4f969a18
Melissa <memeredith@csumb.edu>
2024-05-01 20:38:57 -0700

    fix imports

--- methlotype/regionalMethylation.py
+++ methlotype/regionalMethylation.py
@@ -13,13 +13,13 @@ from sknetwork.data import from_edge_list
 from functools import reduce
 from scipy.spatial.distance import hamming
 
-# from utils import bam_functions as bf
-# from utils import graph_utils as graphing
-# from utils import methylBam as MethylBam
+from utils import bam_functions as bf
+from utils import graph_utils as graphing
+from utils import methylBam as MethylBam
 
 # static bam functions
 import sys
-
+"""
 # append the path of the current directory
 sys.path.append(".")
 # from .bam_functions import *
@@ -27,7 +27,7 @@ sys.path.append(".")
 from methlotype.utils import methylBam as mB
 from methlotype.utils import bam_functions as bf
 from methlotype.utils import graph_utils as graphing
-
+"""
 
 def get_cov_at_each_pos(df, sample_name):
     """ store the frequency of each methylation character at each position for easy look up """
